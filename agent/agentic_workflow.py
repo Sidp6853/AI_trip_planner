a1af8a2922a60216e57e3eb95e42c192a98402b3
Sidp6853 <siddhip568@gmail.com>
2025-10-28 14:19:39 +0530

    Model loader code updated

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,25 @@
+from utils.model_loaders import ModelLoader
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.calculator_tool import CalculatorTool
+from tools.currency_convertor_tool import CurrencyConvertorTool
+from tools.calculator_tool import CalculatorTool
+from tools.arithmetic_tool import ArithmeticTool
+from prompt_library import SYSYTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_conditions
+
+
+class GraphBuilder():
+
+    def __init__(self):
+        
+        pass
+
+    def agent_fucntion(self):
+        pass
+    
+    def build_grapg(self):
+        pass
+    def __call__(self):
+        pass
